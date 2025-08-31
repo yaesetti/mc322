4f5f1a727e2a69889d39652c4f7a9fd6f3acc4b1
Victor Yaegashi Setti <victorminigames@hotmail.com>
2025-08-31 13:47:24 -0300

    idk

--- lab01/src/Hero.java
+++ lab01/src/Hero.java
@@ -4,12 +4,25 @@ public abstract class Hero extends Character {
     private int level = 1;
     private int exp = 0;
 
+    // Array that indicates the amount of exp a Hero needs to be the level it's position
+    // indicates. For example: 2700 exp is necessary to be at level 4.
+    private int[] expPerLevel = {0, 0, 300, 900, 2700, 6500, 14000, 23000, 34000, 48000,
+                                 64000};
+
     public Hero(String name, int healthPoints, int willPoints, int strength) {
         super(name, healthPoints, willPoints, strength);
     }
 
     public void gainExp(int exp) {
         this.exp += exp;
+
+        // Verify if after gaining exp the Hero can level up.
+        
+        // It needs to be a loop because the player can level up multiple times
+        // if the amount of exp gained is big enough.
+        while (this.exp >= expPerLevel[this.level]) {
+            this.level++;
+        }
     }
 
     @Override
