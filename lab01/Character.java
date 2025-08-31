7f6510eb36ce2858b2d0ac56380aa6d5691d959d
Victor Yaegashi Setti <victorminigames@hotmail.com>
2025-08-31 09:18:07 -0300

    Finished class Character and starting class Hero

---
+++ lab01/Character.java
@@ -0,0 +1,37 @@
+package lab01;
+
+public abstract class Character {
+    private String name;
+    private int healthPoints;
+    private boolean isKnocked;
+    private int willPoints;
+    private int strength;
+
+    public Character(String name, int healthPoints, int willPoints, int strength) {
+        this.name = name;
+        this.healthPoints = healthPoints;
+        this.isKnocked = false;
+        this.willPoints = willPoints;
+        this.strength = strength;
+    }
+
+    public void receiveDamage(int damage) {
+        if (this.healthPoints - damage <= 0) {
+            this.healthPoints = 0;
+            this.isKnocked = true;
+            return;
+        }
+        this.healthPoints -= damage;
+    }
+
+    public void printStatus() {
+        System.out.printf("Name: %s\n", this.name);
+        System.out.printf("Heatlh Points: %d\n", this.healthPoints);
+        System.out.printf("Will Points: %d\n", this.willPoints);
+        System.out.printf("strength: %d\n", this.strength);
+    }
+
+    public void attack(Character target) {
+        target.receiveDamage(4 + strength);
+    }
+}
---
+++ lab01/Hero.java
@@ -0,0 +1,7 @@
+package lab01;
+
+public class Hero extends Character {
+    public Hero(String name, int healthPoints, int willPoints, int strength) {
+        super(name, healthPoints, willPoints, strength);
+    }
+}
