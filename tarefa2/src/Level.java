7adaed4aab07c79aa367ce529457ffcb71550038
Victor Yaegashi Setti <victorminigames@hotmail.com>
2025-09-11 21:41:41 -0300

    fixing Level.java

--- tarefa2/src/Level.java
+++ tarefa2/src/Level.java
@@ -1,36 +1,31 @@
 import java.util.List;
 
 public abstract class Level {
-    private int challenge;
-    private final String overview;
-    private List<Monster> monsters;
+    private final int challenge;
+    private final String name;
+    private final String description;
+    private final List<Monster> monsters;
 
-    public Level (int challenge, String  overview, List monsters) {
+    public Level (int challenge, String name, String description, List<Monster> monsters) {
         this.challenge = challenge;
-        this.overview = overview;
+        this.name = name;
+        this.description = description;
         this.monsters = monsters;
     }
-    //
-
-
 
     public int getChallenge() {
         return this.challenge;
     }
-    public String getOverview() {
-        return this.overview;
+
+    public String getName() {
+        return this.name;
     }
-    public List getMonsters() {
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public List<Monster> getMonsters() {
         return this.monsters;
     }
-
-    // Informacoes da fase
-    public void getInfos() {
-        System.out.println("Level" + challenge + " - " + overview);
-        System.out.println("Monsters: ");
-        for (Monster m: monsters) {
-            System.out.println(m.getStatus);
-        }
-    }
-
 }
