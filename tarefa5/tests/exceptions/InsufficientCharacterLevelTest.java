4e27573fb2d1be172cb75c6748866c9682c987a1
Victor Yaegashi Setti <victorminigames@hotmail.com>
2025-10-03 09:14:43 -0300

    implemented InsufficientCharacterLevel exception test

---
+++ tarefa5/tests/exceptions/InsufficientCharacterLevelTest.java
@@ -0,0 +1,39 @@
+package exceptions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import characters.heroes.Mutant;
+import items.weapons.Sword;
+
+public class InsufficientCharacterLevelTest {
+    private final PrintStream originalSystemErr = System.err;
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @AfterEach
+    void restoreStream() {
+        System.setErr(originalSystemErr);
+    }
+
+    @Test
+    public void testInsufficientCharacterLevel() {
+        System.setErr(new PrintStream(errContent));
+
+        // Instantiating a Test Hero
+        Mutant hero = new Mutant("Test Hero", 100, 0, 10);
+        Sword sword = new Sword("Test Sword", 7, 7, hero);
+        try {
+            hero.setWeapon(sword);
+        } catch (InsufficientCharacterLevel e) {
+            System.err.println(e.getMessage());
+        }
+
+        String errorOutput = errContent.toString();
+        assertTrue(errorOutput.contains("Error: Character's level too low to equip this item"));
+    }
+}
