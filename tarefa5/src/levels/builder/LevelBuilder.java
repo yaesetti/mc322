db0878037a7b6cfa4279b4dc1c0c98cc16e941ce
Iago Lucini da Silva <iag.lucini@gmail.com>
2025-10-05 01:18:18 -0300

    Doc: LevelBuilder

--- tarefa5/src/levels/builder/LevelBuilder.java
+++ tarefa5/src/levels/builder/LevelBuilder.java
@@ -5,6 +5,13 @@ import java.util.ArrayList;
 import levels.CombatLevel;
 import levels.Difficulty;
 
+/*
+ * Gera uma lista com niveis de combate
+ * 
+ * @param hero heroi que esta enfrentando os desafios
+ * @param num numero de cenarios
+ * @param difficulty dificuldade dos cenarios
+ */
 public interface LevelBuilder {
     public ArrayList<CombatLevel> generateLevels(Hero hero, int num, Difficulty difficulty);
 }
