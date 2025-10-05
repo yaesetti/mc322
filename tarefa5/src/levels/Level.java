6d8d35d6969b9889ea9d0027c38f002b0911b292
Iago Lucini da Silva <iag.lucini@gmail.com>
2025-10-05 01:14:49 -0300

    Doc: Scenario

--- tarefa5/src/levels/Level.java
+++ tarefa5/src/levels/Level.java
@@ -2,11 +2,23 @@ package levels;
 
 import levels.scenarios.Scenario;
 
+/*
+ * Interface de level
+ */
 public interface Level {
+    /*
+     * Comeca a fase
+     */
     public void start();
 
+    /*
+     * Se o level esta completo ou nao
+     */
     public boolean getIsCompleted();
 
+    /*
+     * 
+     */
     public Scenario getScenario();
 
     public int getTurnCounter();
